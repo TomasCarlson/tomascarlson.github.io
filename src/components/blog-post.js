ac29ba804f5c96e700403a7eb5ae86b9fcd19388
TomasCarlson <34401946+TomasCarlson@users.noreply.github.com>
2021-06-10 00:17:04 -0400

    added ender

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -29,6 +29,7 @@ class BlogPost extends React.Component {
       <div className="w-100 h-100 d-flex" style={{ color: "#E6ECED", backgroundColor: "#B0A282", zIndex: "4" }}>
         <div className="w-100 text-center pr-4 pl-4 pr-sm-5 pl-sm-5 pt-3 pb-2">
           {this.props.children}
+          <hr></hr>
         </div>
       </div>
     )
